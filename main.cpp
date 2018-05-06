e74172af5d22be586a038eafc5b4b87b5d35ce03
DogtorPhD <lectrodic@gmail.com>
2018-05-06 02:41:29 -0400

    Fixed missing screen vars

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 int SCREEN_WIDTH = 640;
 int SCREEN_HEIGHT = 480;
 
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
+
 bool init(){
   //Initialization flag
   bool success = true;
