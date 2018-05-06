fa6a0a29749f95fc0fe71116b60cd3c5f5b83a87
DogtorPhD <lectrodic@gmail.com>
2018-05-06 02:33:07 -0400

    Init completed

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <SDL.h>
+#include <stdio.h>
+
+bool init(){
+  //Initialization flag
+  bool success = true;
+  //Initialize SDL
+  if(SDL_Init(SDL_INIT_VIDEO)<0){
+    printf("SDL could not initialize! SDL_Error:%s\n", SDL_GetError());
+    return false;
+  }
+  //Create window
+  gWindow = SDL_CreateWindow("SDLTutorial",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH,SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
+  if (gWindow == NULL){
+    printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    return false;
+  }
+  //Get widnow SDL_Surface
+  gScreenSurface = SDL_GetWindowSurface(gWindow);
+}
